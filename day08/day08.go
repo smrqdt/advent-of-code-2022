92e3b353000c092faad92b0f7672b786a03d86d6
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2022-12-08 07:37:48 +0100

    Day 08, Part 2

--- day08/day08.go
+++ day08/day08.go
@@ -11,6 +11,7 @@ const InputFile = "day08/input.txt"
 func main() {
 	forest := readInput()
 	part1(forest)
+	part2(forest)
 }
 
 func part1(forest [][]int) {
@@ -62,19 +63,91 @@ func part1(forest [][]int) {
 	}
 
 	visibleCount := 0
+	printColHeader(len(visible[0]))
+
 	for row := range visible {
+		fmt.Printf("%2d  ", row)
 		for col := range visible[0] {
 			if visible[row][col] {
-				fmt.Print("X")
+				fmt.Print("T")
 				visibleCount++
 			} else {
-				fmt.Print("o")
+				fmt.Print(".")
 			}
 		}
+		fmt.Printf("  %2d", row)
 		fmt.Println()
 	}
+	printColHeader(len(visible[0]))
 
 	fmt.Printf("(Part 1) %d trees are visible from outside the grid. \n", visibleCount)
+}
+
+func part2(forest [][]int) {
+	lastRow := len(forest) - 1
+	lastCol := len(forest[0]) - 1
+
+	// scenicScores := make([][]int, len(forest))
+	// for i := range scenicScores {
+	// 	scenicScores[i] = make([]int, len(forest[0]))
+	// }
+
+	maxScenicScore := 0
+	maxScoreRow, maxScoreCol := 0, 0
+	for row := range forest {
+		for col := range forest {
+			height := forest[row][col]
+			scenicScore := 1
+
+			// check left
+			distance := 0
+			for leftCol := col - 1; leftCol >= 0; leftCol-- {
+				distance++
+				if forest[row][leftCol] >= height {
+					break
+				}
+			}
+			scenicScore *= distance
+
+			// check top
+			distance = 0
+			for topRow := row - 1; topRow >= 0; topRow-- {
+				distance++
+				if forest[topRow][col] >= height {
+					break
+				}
+			}
+			scenicScore *= distance
+
+			// check right
+			distance = 0
+			for rightCol := col + 1; rightCol <= lastCol; rightCol++ {
+				distance++
+				if forest[row][rightCol] >= height {
+					break
+				}
+			}
+			scenicScore *= distance
+
+			// check bottom
+			distance = 0
+			for bottomRow := row + 1; bottomRow <= lastRow; bottomRow++ {
+				distance++
+				if forest[bottomRow][col] >= height {
+					break
+				}
+			}
+			scenicScore *= distance
+			if maxScenicScore < scenicScore {
+				maxScenicScore = scenicScore
+				maxScoreRow = row
+				maxScoreCol = col
+			}
+			// scenicScores[row][col] = scenicScore
+
+		}
+	}
+	fmt.Printf("(Part 2) The best scenic score is %d at Row %d, Col %d. \n", maxScenicScore, maxScoreRow, maxScoreCol)
 }
 
 func readInput() [][]int {
@@ -103,9 +176,22 @@ func readInput() [][]int {
 		panic(err)
 	}
 
-	for row := range lines {
-		fmt.Printf("%d\n", lines[row])
-	}
+	// for row := range lines {
+	// 	fmt.Printf("%d\n", lines[row])
+	// }
 
 	return lines
+}
+
+func printColHeader(cols int) {
+	fmt.Print("    ")
+	for col := 0; col < cols; col++ {
+		fmt.Printf("%d", col/10)
+	}
+	fmt.Println()
+	fmt.Print("    ")
+	for col := 0; col < cols; col++ {
+		fmt.Printf("%d", col%10)
+	}
+	fmt.Println()
 }
