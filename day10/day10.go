d4422027869e11f81eb03ac7183ca285372b93ea
Simon Marquardt (smrqdt) <simon.marquardt@posteo.de>
2022-12-10 07:33:51 +0100

    Day 10, Part 2

--- day10/day10.go
+++ day10/day10.go
@@ -13,10 +13,6 @@ import (
 const InputFile = "day10/input.txt"
 
 func main() {
-	part1()
-}
-
-func part1() {
 	f, err := os.Open(InputFile)
 	if err != nil {
 		panic(fmt.Sprintf("Could not open file %v \n", err))
@@ -24,11 +20,21 @@ func part1() {
 	defer f.Close()
 
 	scanner := bufio.NewScanner(f)
+
+	// Part 1
 	var signalStrengths int
-	cpu := NewCPU([]int{20, 60, 100, 140, 180, 220}, func(cpu *CPU) {
-		fmt.Printf("Cycle: %d, Register: %d \n", cpu.CurrentCycle, cpu.Register)
-		signalStrengths += cpu.CurrentCycle * cpu.Register
-	})
+	watchCycles := []int{20, 60, 100, 140, 180, 220}
+	watchFn := func(cpu *CPU) {
+		if slices.Contains(watchCycles, cpu.Time) {
+			// fmt.Printf("Cycle: %d, Register: %d \n", cpu.Time, cpu.Register)
+			signalStrengths += cpu.Time * cpu.Register
+		}
+	}
+
+	// Part 2
+	crt := make(CRT, 40*6)
+
+	cpu := NewCPU([]func(*CPU){watchFn, crt.Draw})
 
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -40,27 +46,30 @@ func part1() {
 	}
 
 	fmt.Printf("(Part 1) Sum of Signal Strengths: %d \n", signalStrengths)
+	fmt.Println("(Part 2) CRT Image:")
+	crt.Print()
 }
 
 type CPU struct {
-	Register     int
-	CurrentCycle int
-	WatchCycles  []int
-	WatchFn      func(*CPU)
+	Register int
+	Time     int
+	WatchFn  []func(*CPU)
 }
 
-func NewCPU(watchCycles []int, watchFn func(*CPU)) *CPU {
-	return &CPU{1, 1, watchCycles, watchFn}
+func NewCPU(watchFn []func(*CPU)) *CPU {
+	return &CPU{1, 1, watchFn}
 }
 
 func (cpu *CPU) cycle() {
-	if slices.Contains(cpu.WatchCycles, cpu.CurrentCycle) {
-		cpu.WatchFn(cpu)
+	// fmt.Printf("Cycle %d \n", cpu.Time)
+	for _, fn := range cpu.WatchFn {
+		fn(cpu)
 	}
-	cpu.CurrentCycle++
+	cpu.Time++
 }
 
 func (cpu *CPU) exec(line string) {
+	// fmt.Printf("Exec %s \n", line)
 	fields := strings.Fields(line)
 	switch fields[0] {
 	case "addx":
@@ -72,7 +81,7 @@ func (cpu *CPU) exec(line string) {
 	case "noop":
 		cpu.noop()
 	}
-	// fmt.Printf("exec %s, cycle %d, register %d \n", line, cpu.CurrentCycle, cpu.Register)
+	// fmt.Printf("Finish %s (Register value %d) \n", line, cpu.Register)
 }
 
 func (cpu *CPU) addx(val int) {
@@ -83,4 +92,21 @@ func (cpu *CPU) addx(val int) {
 
 func (cpu *CPU) noop() {
 	cpu.cycle()
+}
+
+type CRT []byte
+
+func (crt CRT) Draw(cpu *CPU) { // Direct Register Access!!11
+	if col := (cpu.Time - 1) % 40; col >= cpu.Register-1 && col <= cpu.Register+1 {
+		crt[cpu.Time-1] = '#'
+	} else {
+		crt[cpu.Time-1] = ' '
+	}
+	// fmt.Printf("Drawing Pos %d: %q \n", cpu.Time%40, crt[cpu.Time-1])
+}
+
+func (crt CRT) Print() {
+	for i := 0; i < len(crt); i += 40 {
+		fmt.Println(string(crt[i : i+40]))
+	}
 }
